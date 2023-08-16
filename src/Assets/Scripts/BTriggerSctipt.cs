818d504da35e77e114645a6f2efedab0ff28becf
SugarWind <g2127037@st.t-kougei.ac.jp>
2023-08-17 01:47:36 +0900

    カメラ追従と画面外の弾丸処理 カメラの外に行った弾丸はDestroyされます。

--- src/Assets/Scripts/BTriggerSctipt.cs
+++ src/Assets/Scripts/BTriggerSctipt.cs
@@ -6,10 +6,15 @@ public class BTriggerSctipt : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "wall")
+        if (collision.gameObject.tag == "wall")
         {
             Debug.Log("Ç†ÇΩÇË");
             Destroy(this.gameObject);
         }
+    }
+
+    void OnBecameInvisible() //âÊñ äOÇÃíeä€èàóù
+    {
+        Destroy(this.gameObject);
     }
 }
