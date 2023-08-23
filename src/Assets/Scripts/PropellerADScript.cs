c051161c67c9f71bd20cad7669ee30dde835e369
SugarWind <g2127037@st.t-kougei.ac.jp>
2023-08-23 21:53:12 +0900

    プロペラの実装 とりあえず加速だけ実装 減速も同じようにやる

---
+++ src/Assets/Scripts/PropellerADScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerADScript : MonoBehaviour
+{
+    public GameObject PropellerArea;
+    public AreaEffector2D Paf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+         Paf = PropellerArea.GetComponent<AreaEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Acammo")
+        {
+            Debug.Log("ŒŸ’mƒvƒƒyƒ‰");
+
+            Paf.forceMagnitude += 2f;
+
+            if(Paf.forceMagnitude > 20)
+            {
+                Paf.forceMagnitude = 20;
+            }
+            
+        }
+    }
+}
