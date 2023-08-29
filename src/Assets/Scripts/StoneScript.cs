f4be793e2554f56ad823028bda1c9cffb982fca5
SugarWind <g2127037@st.t-kougei.ac.jp>
2023-08-29 23:03:40 +0900

    岩の実装（80%？） 重力を変化させるのではなくy軸の速度ベクトルを変化する方向に変えた。

--- src/Assets/Scripts/StoneScript.cs
+++ src/Assets/Scripts/StoneScript.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class StoneScript : MonoBehaviour
 {
+    private Rigidbody2D Srb;
+    private float GravitySpeed = - 8f; //—Ž‰º‘¬“x
+
+    private void Start()
+    {
+        Srb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Acammo")
         {
             Debug.Log("‰Á‘¬’e~Šâ");
+            GravitySpeed -= 1.0f; //—Ž‰º‘¬“x‘‰Á
         }
 
         if (collision.gameObject.tag == "Dcammo")
         {
             Debug.Log("Œ¸‘¬’e~Šâ");
+            GravitySpeed += 1.0f; //—Ž‰º‘¬“xŒ¸Š
         }
     }
 
-   
+    private void Update()
+    {
+        if(GravitySpeed > 0.0f)
+        {
+            GravitySpeed = -1.0f;
+        }
+
+
+        Vector2 newVelocity = new Vector2(Srb.velocity.x, GravitySpeed); 
+        Srb.velocity = newVelocity;
+    }
+
+
 }
