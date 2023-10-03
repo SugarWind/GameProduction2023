91fd407e691326db29c4d6475f34bd45c484fe9d
MEN1213 <g212033@st.t-kougei.ac.jp>
2023-10-03 21:52:18 +0900

    新しいシーンの追加

---
+++ src/Assets/script/Rutrun.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rutrun : MonoBehaviour
+{
+    public void LodingNewSence()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
