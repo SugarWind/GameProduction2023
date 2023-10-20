c53731f66d9db3a929d186db50ae56e3270eb112
SK2127043 <g2127043@st.t-kougei.ac.jp>
2023-10-21 02:03:15 +0900

    背景変更

---
+++ src/Assets/Scripts/ParallaxCameraFlowLayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxCameraFlowLayer : MonoBehaviour
+{
+    [SerializeField] Transform _cameraTransfrom;    // 追従対象のカメラ
+    [SerializeField] float _followFactor;   // カメラに追従する程度(1: カメラと同じ移動量 0: 移動しない)
+
+    Vector2 _previousCameraPos;
+
+    private void Update()
+    {
+        Vector2 currentPos = _cameraTransfrom.position;
+        var deltaPos = currentPos - _previousCameraPos;
+        _previousCameraPos = currentPos;
+        var calcedPos = deltaPos * _followFactor;
+        transform.AddLocalPos((Vector2)calcedPos);
+    }
+}
+
+// ユーティリティ
+public static class ParallaxCameraFlowLayerExtensions
+{
+    public static void AddLocalPos(this Transform self, in Vector2 pos)
+    {
+        Vector2 vec = self.localPosition;
+        vec.x += pos.x;
+        //vec.y += pos.y;
+        //vec.x = Mathf.Round(vec.x * 10) / 10;
+        self.localPosition = vec;
+    }
+}
