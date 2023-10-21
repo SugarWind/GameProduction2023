5584f5efd9a74a69958c05fb22d870a7938b736c
g2127043 <g2127043@st.t-kougei.ac.jp>
2023-10-21 12:13:01 +0900

    change

---
+++ src/Assets/Scripts/SimpleMoveObject.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMoveObject : MonoBehaviour
+{
+    [SerializeField] private Transform _objDestination;
+
+    [SerializeField] private float _moveSpeed = 3.0f;
+    [SerializeField] private float _changeSpeed = 2; //ïœâªÇÃî{ó¶
+    [SerializeField] private float _changeScale = 2; //ïœâªÇÃíiäK
+
+    private Rigidbody2D _rbObj;
+    private float _moveSpeed;
+    private float _maxSpeed;    //ç≈çÇë¨ìx
+    private float _minSpeed;    //ç≈í·ë¨ìx
+    private SpriteRenderer _objSprite;
+    public Sprite _objDefault;
+    public Sprite _objAcc;
+    public Sprite _objDec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rbObj = GetComponent<Rigidbody2D>();
+        pressSprite = gameObject.GetComponent<SpriteRenderer>();
+        _moveSpeed = MoveSpeed;
+        _maxSpeed = MoveSpeed * Mathf.Pow(ChangeSpeed, ChangeScale);
+        _minSpeed = MoveSpeed / Mathf.Pow(ChangeSpeed, ChangeScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
