eff94eef9c89440516bf751521bfc4059d9e9268
MEN1213 <g212033@st.t-kougei.ac.jp>
2023-11-14 12:23:33 +0900

    ステージセレクトの修正

---
+++ src/Assets/script/testscript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testscript : MonoBehaviour
+{
+    public int Save_num; // スコア変数
+    public GameObject[] stageSelect = default;
+
+    void Start()
+    {
+        //現在のstage_numを呼び出す
+        Save_num = PlayerPrefs.GetInt("SCORE", 9);
+
+        for (int loop = 0; loop < stageSelect.Length; loop++)
+        {
+            if (loop < Save_num)
+            {
+                stageSelect[loop].SetActive(true);
+            }
+            else
+            {
+                stageSelect[loop].SetActive(false);
+            }
+        }
+    }
+}
