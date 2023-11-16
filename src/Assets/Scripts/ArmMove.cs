97d1472f51c114a539a84e29c1aaef73d9a75998
SK2127043 <g2127043@st.t-kougei.ac.jp>
2023-11-17 02:39:02 +0900

    ArmMoveなどの変更

--- src/Assets/Scripts/ArmMove.cs
+++ src/Assets/Scripts/ArmMove.cs
@@ -45,7 +45,7 @@ public class ArmMove : MonoBehaviour
         armSpriteRenderer.enabled = player.spriteRenderer.enabled;
 
         // マウスのx座標に応じてオブジェクトの左右を反転
-        if (mousePosition.x < playerTransform.position.x && !isHit)
+        if (mousePosition.x < playerTransform.position.x && !isHit && Right)
         {
             // マウスが自機の左側にある場合、自機を左向きにします
             transform.localScale = new Vector3(1, -1, 1);
@@ -55,7 +55,7 @@ public class ArmMove : MonoBehaviour
             secondArmScript.ChangeSpriteLeft();
             Right = false;  
         }
-        else if (mousePosition.x >= playerTransform.position.x && !isHit)
+        else if (mousePosition.x >= playerTransform.position.x && !isHit && !Right)
         {
             // マウスが自機の右側にある場合、自機を右向きにします
             transform.localScale = new Vector3(1, 1, 1);
