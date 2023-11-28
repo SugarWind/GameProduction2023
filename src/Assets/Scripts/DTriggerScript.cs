fce9dcbbb377f4ffec0d291f339c01bf829bf8b9
SK2127043 <g2127043@st.t-kougei.ac.jp>
2023-11-28 14:44:32 +0900

    iroiro

--- src/Assets/Scripts/DTriggerScript.cs
+++ src/Assets/Scripts/DTriggerScript.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class DTriggerScript : MonoBehaviour
 {
-    [SerializeField] private GameObject _destroyedPrefab;
+    [SerializeField] private AnimationClip _decAnimation;
+    [SerializeField] private AnimationClip _explosionAnimation;
+    private GameObject _effectObject;
+    private Animator _decAnimator;
+    private Rigidbody _rb;
+    void Start()
+    {
+        _effectObject = transform.Find("bullet_dec_effect").gameObject;
+        _decAnimator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "wall")
         {
             Debug.Log("Ç†ÇΩÇË");
-            Instantiate(_destroyedPrefab, transform.position, transform.rotation);
-            Destroy(this.gameObject);
+            Destroy(_effectObject);
+            _decAnimator.SetTrigger("collisionTrigger");
         }
 
         if (collision.gameObject.tag != "Dcammo" && collision.gameObject.tag != "Acammo" && collision.gameObject.tag != "Arm" && collision.gameObject.tag != "Player" && collision.gameObject.tag != "Jump" && collision.gameObject.tag != "Trigger")
         {
-            Instantiate(_destroyedPrefab, transform.position, transform.rotation);
-            Destroy(this.gameObject);
+            Destroy(_effectObject);
+            _decAnimator.SetTrigger("collisionTrigger");
         }
     }
 
     void OnBecameInvisible() //âÊñ äOÇÃíeä€èàóù
+    {
+        Destroy(this.gameObject);
+    }
+
+    void OnAnimationFinish()
     {
         Destroy(this.gameObject);
     }
