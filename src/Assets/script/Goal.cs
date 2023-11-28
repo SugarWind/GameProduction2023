030c5f12ca9d27b37da273afdb62244f0ee87071
MEN1213 <g212033@st.t-kougei.ac.jp>
2023-11-28 14:06:25 +0900

    一部修正

--- src/Assets/script/Goal.cs
+++ src/Assets/script/Goal.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class Goal : MonoBehaviour
 {
     //ゴールした後にステージセレクトへ移動
+
+    
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -15,13 +17,12 @@ public class Goal : MonoBehaviour
             playerScript.DeleteGoal();
             Debug.Log("ゴール");
             SceneManager.LoadScene("StageSlect");
+
+            //PlayerPrefsのSCOREに3という値を入れる
+            PlayerPrefs.SetInt("SCORE", 2);
+            //PlayerPrefsをセーブする         
+            PlayerPrefs.Save();
         }
     }
-    //public void StageClear()
-    //{
-    //    PlayerPrefsのSCOREに3という値を入れる
-    //    PlayerPrefs.SetInt("SCORE", 2);
-    //    PlayerPrefsをセーブする
-    //    PlayerPrefs.Save();
-    //}
+ 
 }
--- src/Assets/script/select.cs
+++ src/Assets/script/select.cs
@@ -6,6 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class Select : MonoBehaviour
 {
+    public int Save_num; // スコア変数
+    public GameObject[] stageSelect = default;
+
+    void Start()
+    {
+        //現在のstage_numを呼び出す
+        Save_num = PlayerPrefs.GetInt("SCORE", 9);
+
+        for (int loop = 0; loop < stageSelect.Length; loop++)
+        {
+            if (loop < Save_num)
+            {
+                stageSelect[loop].SetActive(true);
+            }
+            else
+            {
+                stageSelect[loop].SetActive(false);
+            }
+        }
+    }
+
     //問題ありだったためtestscpirtを採用
     //ステージ制限.
     //[SerializeField] private Button[] _stageButton;
