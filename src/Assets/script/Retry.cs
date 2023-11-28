6d64d1d7237f0f1f57355ddf1b07d5d8db0c8410
MEN1213 <g212033@st.t-kougei.ac.jp>
2023-11-28 11:05:33 +0900

    ステージにUI追加

--- src/Assets/script/Retry.cs
+++ src/Assets/script/Retry.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class Retry : MonoBehaviour
 {
     //string;
@@ -15,6 +16,7 @@ public class Retry : MonoBehaviour
     // Update is called once per frame
     public void RetryButton()
     {
-        SceneManager.LoadScene("Stage2");
+        //SceneManager.LoadScene("Stage2");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
