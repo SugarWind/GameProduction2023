b2a53718b05868d607c79c2b87cb27b552115c16
SK2127043 <g2127043@st.t-kougei.ac.jp>
2023-12-12 16:17:31 +0900

    c

--- src/Assets/Scripts/PressMachineScript.cs
+++ src/Assets/Scripts/PressMachineScript.cs
@@ -9,18 +9,19 @@ public class PressMachineScript : MonoBehaviour
 
     [SerializeField] private float MoveSpeed = 10.0f;
     [SerializeField] private float ChangeSpeed = 2.0f; //変化の倍率
-    [SerializeField] private float ChangeScale = 2.0f; //変化の段階
+    [SerializeField] private float ChangeScale = 1.0f; //変化の段階
     [SerializeField] private float WaitTime = 1.0f;
 
     private int direction = -1;
-
     private float _moveSpeed;
     private float _maxSpeed;    //最高速度
     private float _minSpeed;    //最低速度
+    private float _underPositionY;
     private SpriteRenderer pressSprite;
     private bool _isStop;
     private bool _isHitAcc;
     private bool _isHitDec;
+    private bool _hasPressed;
     [SerializeField] private Sprite PressDefault;
     [SerializeField] private Sprite PressAcc;
     [SerializeField] private Sprite PressDec;
@@ -30,6 +31,7 @@ public class PressMachineScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if(!TopPos) TopPos = this.transform;
         rb = GetComponent<Rigidbody2D>();
         pressSprite = gameObject.GetComponent<SpriteRenderer>();
         _moveSpeed = MoveSpeed;
@@ -42,22 +44,14 @@ public class PressMachineScript : MonoBehaviour
     {
         if (!_isStop)
         {
-            rb.MovePosition(new Vector2(transform.position.x, transform.position.y + MoveSpeed * Time.fixedDeltaTime * direction));
-
-            if (transform.position.y > TopPos.position.y + 8)
-            {
-                direction *= -direction;
-            }
+            rb.MovePosition(new Vector2(transform.position.x, transform.position.y + MoveSpeed * Time.fixedDeltaTime * direction));  
         }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "floor")
+        if (this.transform.position.y > TopPos.position.y && !_hasPressed)
         {
-            _isStop = true;
-            Invoke("ReturnPress", WaitTime);
-            Debug.Log("プレス接触");
+            _hasPressed = true;
+            StartCoroutine(ChangeBoolCoroutine(_hasPressed, 0.1f));
+            this.transform.position = new Vector2(this.transform.position.x, TopPos.position.y);
+            direction = -direction;
         }
     }
 
@@ -66,7 +60,7 @@ public class PressMachineScript : MonoBehaviour
         if (collision.gameObject.tag == "Acammo" && MoveSpeed < _maxSpeed && !_isHitAcc)
         {
             _isHitAcc = true;
-            Invoke("AccAmmoHitWaitTime", 0.1f);
+            StartCoroutine(ChangeBoolCoroutine(_isHitAcc, 0.1f));
             MoveSpeed *= ChangeSpeed;
             WaitTime /= ChangeSpeed;
             Debug.Log("プレス加速");
@@ -74,7 +68,7 @@ public class PressMachineScript : MonoBehaviour
         if (collision.gameObject.tag == "Dcammo" && MoveSpeed > _minSpeed && !_isHitDec)
         {
             _isHitDec = true;
-            Invoke("DecAmmoHitWaitTime", 0.1f);
+            StartCoroutine(ChangeBoolCoroutine(_isHitDec, 0.1f));
             MoveSpeed /= ChangeSpeed;
             WaitTime *= ChangeSpeed;
             Debug.Log("プレス減速");
@@ -89,14 +83,10 @@ public class PressMachineScript : MonoBehaviour
         _isStop = false;
     }
 
-    private void AccAmmoHitWaitTime()
+    public IEnumerator ChangeBoolCoroutine(bool _isBool, float num)
     {
-        _isHitAcc = false;
-    }
-
-    private void DecAmmoHitWaitTime()
-    {
-        _isHitDec = false;
+        _isBool = false;
+        yield return new WaitForSeconds(num);
     }
 
     private void ChangeSprite()    //加減速状態に応じてプレス機のスプライトを変更
@@ -113,5 +103,21 @@ public class PressMachineScript : MonoBehaviour
         {
             pressSprite.sprite = PressDec;
         }
+    }
+
+    public void OnCollisionReturn()
+    {
+        if (!_hasPressed)
+        {
+            _hasPressed = true;
+            _underPositionY = Mathf.Round(this.transform.position.y);
+        }
+        if (this.transform.position.y < _underPositionY)
+        {
+            this.transform.position = new Vector2(this.transform.position.x, _underPositionY);
+        }
+        _isStop = true;
+        Invoke("ReturnPress", WaitTime);
+        Debug.Log("プレス接触");
     }
 }
